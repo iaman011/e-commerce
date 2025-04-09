43b779d3ab8c6bd346e69e835b5a0a25e30c421f
iaman011 <iaman.singh011@gmail.com>
2025-04-09 19:40:06 +0530

    changes commit

--- e-commerce/app/page.tsx
+++ e-commerce/app/page.tsx
@@ -1,3 +1,4 @@
+import { Carousel } from "@/components/carousel";
 import { Button } from "@/components/ui/button";
 import { stripe } from "@/lib/stripe";
 import Image from "next/image";
@@ -8,19 +9,30 @@ export default async function Home() {
   const products = await stripe.products.list({
     expand: ["data.default_price"],
     limit: 5,
-  })
+  });
 
   console.log(products);
   return (
     <div>
-      <section>
-        <div>
-          <div>
-            <h2>Welcome to My Ecommerce</h2>
-            <p>Discover the latest products at the best prices.</p>
+    <section className="rounded bg-neutral-100 py-8 sm:py-12">
+      <div className="mx-auto grid grid-cols-1 items-center justify-items-center gap-8 px-8 sm:px-16 md:grid-cols-2">
+        <div className="max-w-md space-y-4">
+          <h2 className="text-3xl font-bold tracking-tight md:text-4xl">
+              Welcome to My Ecommerce
+              </h2>
+            <p className="text-neutral-600">Discover the latest products at the best prices.</p>
             {/* importing Button to use shadcn library and inside it instead of onClick we use link to maintain the server side rendering as onClick requires state_var hooks and renders on client side component */}
-            <Button asChild variant="default">  
-              <Link href="/products">Browse All Products</Link>
+            <Button 
+            asChild 
+            variant="default"
+            className="inline-flex items-center justify-center rounded-full px-6 py-3 bg-black text-white"
+            >  
+              <Link 
+              href="/products"
+              className="inline-flex items-center justify-center rounded-full px-6 py-3"
+              >
+                Browse All Products
+              </Link>
 
             </Button>
           </div>
@@ -29,6 +41,9 @@ export default async function Home() {
           <Image alt="Banner Image" width={450} height={450} src={products.data[0].images[0]} />
 
         </div>
+      </section>
+      <section className="py-8">
+        <Carousel products={products.data}/>
       </section>
     </div>
   );
--- e-commerce/next.config.ts
+++ e-commerce/next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // it is essential to while fetch image from API, we need to provide the domain
+  images: {
+    domains: ["files.stripe.com"],
+  }
 };
 
 export default nextConfig;
